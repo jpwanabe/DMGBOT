29633236b33d5380d8d82c2d0c08ce8fb88a4b5b
Nicolas <BonzoBonham@users.noreply.github.com>
2019-05-01 16:38:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ function textchannelupdate(){
         lastMessage.edit(`${playerlist}`)
         .then(msg => console.log(`New message content: ${msg}`))
         .catch(console.error);
-        
+        statuschat.send("-------------------------------------------------------------");
 
     }).catch((error) => {
         console.log("Server is offline");
@@ -83,12 +83,11 @@ bot.on("ready", async message => {
     bot.setInterval(update,30000);
     bot.setInterval(voicechannelupdate,30000);
     statuschat = bot.channels.get("573022289931796511");
-    statuschat.send("***DIRECT CONNECT TO SERVER LINK***: Click this link to open up Garry's Mod and connect to the server");
+    statuschat.send("***Click this link to open up Garry's Mod and connect to the server!***");
     statuschat.send("steam://connect/66.151.244.2:27015");
-    statuschat.send("-------------------------------------------------------------");
+    statuschat.send("----------------------**ONLINE PLAYERS**----------------------");
     statuschat.send("Initializing...");
     bot.setInterval(textchannelupdate,30000);
-    statuschat.send("-------------------------------------------------------------");
 });
 
 //List of commands that can be called to the bot
