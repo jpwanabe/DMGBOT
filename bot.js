52b6a5a4563692864683234e300ecb34c8909618
Nicolas <BonzoBonham@users.noreply.github.com>
2019-05-01 13:03:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ function textchannelupdate(){
         playerArray = state.players;
         console.log("getting players...")
         while (i < playerArray.length) {
-            playerlist = playerlist + playerArray[i] + `
+            playerlist = playerlist + playerArray[i].name + `
 `;
             i++;
         }
